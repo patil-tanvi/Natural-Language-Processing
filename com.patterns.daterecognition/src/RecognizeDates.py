5f7aa97ccc74f583869abdae93361692ad18848e
Tanvi Santosh Patil <patil.tanvi@gmail.com>
2015-02-04 17:01:44 -0600

    Regex modified to identify all dates

--- com.patterns.daterecognition/src/RecognizeDates.py
+++ com.patterns.daterecognition/src/RecognizeDates.py
@@ -11,8 +11,13 @@ fileText = getTextFromFile()
 fileText = fileText.replace('\n',' ')
 print(fileText)
 
-#matchObj = re.findall(r'[0-3]?[0-9][t][h] [o][f] [January|September]', fileText, flags=0)
+matchObjRegEx1 = re.findall(r"[0-3]?[0-9][a-z]?[a-z]? ?o?f? ?[A-Z][a-z]*,? '?[0-9]*", fileText, flags=0)
+matchObjRegEx2 = re.findall(r"[A-Z][A-Za-z]* [0-9][0-9]?[a-z]?[a-z]?,? [a-z]* ?o?f? ?'?[0-9]*", fileText, flags=0)
 
-#print(matchObj)
+print(matchObjRegEx1)
+print(matchObjRegEx2)
 
-#[0-3]?[0-9][a-z]?[a-z]? of? [A-Za-z]*,? [0-9]*
+#Reg Ex1 : [0-3]?[0-9][a-z]?[a-z]? of? [A-Za-z]*,? [0-9]*
+# Reg Ex 1 Modified : [0-3]?[0-9][a-z]?[a-z]? of? [A-Za-z]*,? ([0-9][0-9][0-9][0-9])?
+
+#Reg Ex 2 : [A-Z][A-Za-z]* [0-9][0-9]?[a-z]*,? [0-9]*
